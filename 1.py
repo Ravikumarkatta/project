1be05b117c4ad38bbfa0a30be67966c59f3da17c
ravikumarkatta <kattaravi000@gmail.com>
2025-04-20 21:56:28 +0530

    new 3.00

--- 1.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('kjv_structured_complete.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-    books = len(data['books'])
-    chapters = sum(len(book['chapters']) for book in data['books'])
-    verses = sum(sum(len(chap['verses']) for chap in book['chapters']) for book in data['books'])
-    print(f"Books: {books}, Chapters: {chapters}, Verses: {verses}")
